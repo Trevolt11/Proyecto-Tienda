fe62ccd29b3a4c5d9a5678c79c92211b2bbc72e8
ismael vasquez <joseismael13vm@gmail.com>
2020-10-11 01:18:32 -0400

    arreglo de error en la carpeta vista interfaz

---
+++ Tienda/Tienda/Modelo/Modelo_Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda.Modelo
+{
+    class Modelo_Categoria
+    {
+        static int ID;
+        static string Nombre;
+        static string Estado;
+
+
+    }
+}
--- Tienda/Tienda/Program.cs
+++ Tienda/Tienda/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tienda.Vista;
 
 namespace Tienda
 {
@@ -16,7 +17,7 @@ namespace Tienda
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Interfaz());
         }
     }
 }
