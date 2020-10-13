933128fe1d34762a47f0cfb651412030da0fb8eb
Robert Caraballo <62614866+Trevolt11@users.noreply.github.com>
2020-10-13 16:07:04 -0400

    Update CategoriaControlador.cs

--- Tienda/Tienda/controlador/CategoriaControlador.cs
+++ Tienda/Tienda/controlador/CategoriaControlador.cs
@@ -1,12 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tienda.Modelo;
+using Tienda.Vista;
 
 namespace Tienda.controlador
 {
     class CategoriaControlador
     {
+        MantenimientoCategorias vista1;
+
+        //Construtor
+
+        public CategoriaControlador(MantenimientoCategorias view)
+        {
+            vista1 = view;
+        }
+
+        public void clienteList()
+        {
+
+        }
+
     }
 }
