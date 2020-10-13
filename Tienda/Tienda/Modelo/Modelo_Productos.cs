db7f1b7505389ad48a27d7b1f520b07f2edd0d8b
Robert Caraballo <62614866+Trevolt11@users.noreply.github.com>
2020-10-13 16:09:22 -0400

    Update Modelo_Productos.cs

--- Tienda/Tienda/Modelo/Modelo_Productos.cs
+++ Tienda/Tienda/Modelo/Modelo_Productos.cs
@@ -1,16 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tienda.Modelo
 {
+
     public class Modelo_Productos
     {
-        //bradigson y roshby modificamos aqui
         int idproductos;
         string nombre;
         string codigo;
@@ -19,14 +20,23 @@ namespace Tienda.Modelo
         string descripcion;
         int idcategoria;
         string estado;
-
+        public Modelo_Productos(int iDPRODUCTOS, string nOMBRE, string cODIGO, string sTOCK, DateTime fECHVENCIMIENTO, string dESCRIPCION, int iDCATEGORIA, string eSTADO)
+        {
+            IDPRODUCTOS = iDPRODUCTOS;
+            NOMBRE = nOMBRE;
+            CODIGO = cODIGO;
+            STOCK = sTOCK;
+            FECHVENCIMIENTO = fECHVENCIMIENTO;
+            DESCRIPCION = dESCRIPCION;
+            IDCATEGORIA = iDCATEGORIA;
+            ESTADO = eSTADO;
+        }
 
         public int IDPRODUCTOS { get; set; }
         public string NOMBRE { get; set; }
         public string CODIGO { get; set; }
         public string STOCK{ get; set; }
-        public string FECHVENCIMIENTO  { get; set; }
-        //public DateTime FECHVENCIMIENTO  { get; set; }
+        public DateTime FECHVENCIMIENTO  { get; set; }
         public string DESCRIPCION { get; set; }
         public int IDCATEGORIA{ get; set; }
         public string ESTADO { get; set; }
